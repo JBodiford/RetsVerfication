f978b54e9eda58e32b7a8cc786660818ccc2a9c1
J.Bodiford <JenniferBodiford@gmail.com>
2016-07-20 20:42:39 -0400

    Json parsing

---
+++ RetsVerification/RetsVerification/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using librets;
+
+
+
+
+namespace RetsVerification
+{
+
+    public class RootObject
+    {
+        public int TenantID { get; set; }
+        public string RetsAssociatedUser { get; set; }
+        public string RetsLoginURL { get; set; }
+        public string RetsUserID { get; set; }
+        public string RetsPassword { get; set; }
+        public double RetsVersion { get; set; }
+
+    }
+     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText(@"c:\Users\jbodiford\Documents\retsLoginProject\C#RetsVerification\RetsCredentials.json");
+            var users = JsonConvert.DeserializeObject<List<RootObject>>(json);
+
+            foreach (var item in users)
+            {
+                Console.WriteLine(users.ToString());
+                Console.ReadLine();
+            }
+
+
+       }
+
+     }
+
+
+}

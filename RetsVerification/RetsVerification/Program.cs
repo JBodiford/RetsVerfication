625f1e765625c1aef7465281aea273bf79a84256
J.Bodiford <JenniferBodiford@gmail.com>
2016-07-26 15:56:52 -0400

    WE HAVE LISTING DATA!  WOOHOO!

--- RetsVerification/RetsVerification/Program.cs
+++ RetsVerification/RetsVerification/Program.cs
@@ -43,10 +43,10 @@ namespace RetsVerification
         Error = 20000
     }
 
-    
      
     class Program
     {
+  
         static void Main(string[] args)
         {
             string json = File.ReadAllText(@"c:\Users\jbodiford\Documents\retsLoginProject\C#RetsVerification\RetsCredentials.json");
@@ -83,7 +83,7 @@ namespace RetsVerification
                        session.SetUserAgentPassword(user.RetsUserAgentPassword);
                    
                    bool loginResult = session.Login(user.RetsUserID, user.RetsPassword);
-                       if (!loginResult)
+                   if (!loginResult)
                        {
                           //Console.WriteLine(session.GetReplyCode());
                            Console.WriteLine("\nLogin to {0}'s RETS Failed at " + DateTime.Now, user.RetsAssociatedUser);
@@ -93,7 +93,9 @@ namespace RetsVerification
                        {
                            Console.WriteLine("\nLogin to {0}'s RETS Succeeded at " + DateTime.Now, user.RetsAssociatedUser);
                            Console.ReadLine();
-                           
+
+                           session.SetDefaultEncoding(EncodingType.RETS_XML_DEFAULT_ENCODING);
+
                            NameValueCollection queryVals = System.Web.HttpUtility.ParseQueryString(user.QueryString);
                            string query = queryVals["Query"];
                            
@@ -102,33 +104,54 @@ namespace RetsVerification
                                search.SetQueryType(SearchRequest.QueryType.DMQL2);
                                search.SetStandardNames(false);
                                search.SetLimit(10);
+                               search.SetFormatType(SearchRequest.FormatType.COMPACT_DECODED);
 
                                if (queryVals["Select"] != null)
                                    search.SetSelect(queryVals["Select"]);
                                SearchResultSet results = session.Search(search);
+                               
                                int replyCode = results.GetReplyCode();
+                               //we got records!
+                               if (replyCode == (int)RetsSearchReplyCode.Success)
+                               {
+                                   Console.WriteLine("Record count: " + results.GetCount());
+                                   Console.ReadLine();
+                                   IEnumerable columns = null;
+                                   while (results.HasNext())
+                                   {
+                                       if (columns == null)
+                                       {
+                                           columns = results.GetColumns();
+                                       }
+                                       foreach (string column in columns)
+                                       {
+                                           Console.WriteLine(column + ": " + results.GetString(column));
+                                       }
+                                       Console.WriteLine();
+                                   }
+                                   session.Logout();
                                // no records found
-                               if (replyCode == (int)RetsSearchReplyCode.NoResults)
-                                    Console.WriteLine(results);
-                                    Console.ReadLine();
-
+                               }
+                               else if (replyCode == (int)RetsSearchReplyCode.NoResults)
+                               {
+                                   Console.WriteLine("{0} - This query returned no results", RetsSearchReplyCode.NoResults);
+                                   Console.ReadLine();
                                // other rets errors
-                               if (replyCode > (int)RetsSearchReplyCode.Error)
+                               }
+                               else if (replyCode > (int)RetsSearchReplyCode.Error)
+                               {
                                    throw new Exception(String.Format("{0}: {1} {2}",
                                        replyCode,
                                        results.GetReplyText(),
                                        search.GetQueryString()));
-
-                                   Console.WriteLine(results);
+                                   Console.WriteLine("{0} - Bummer there was a search error.", RetsSearchReplyCode.Error);
                                    Console.ReadLine();
-                               
+                               }
                            }
                            Console.ReadLine();
                        }
-                     
-                   
 
-                   session.Logout();
+                   
                 }
            } 
            catch (Exception ex)
